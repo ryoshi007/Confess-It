d71209ec7d358d2107ae2a54288747a510ea0669
Yu Xuan <u2102858@siswa.um.edu.my>
2022-05-21 00:07:21 +0800

    1.Create database connection 2.Add json dependency and mysql dependency

--- src/main/java/com/confessit/DatabaseConnection.java
+++ src/main/java/com/confessit/DatabaseConnection.java
@@ -1,4 +1,36 @@
 package com.confessit;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * This class is used to connect to the database
+ */
 public class DatabaseConnection {
+
+    /**
+     * Connection is the session between java application and database
+     */
+    public Connection databaseLink;
+
+    /**
+     * This method is used to get connection from the database
+     *
+     * @return A database link
+     */
+    public Connection getConnection() {
+        String databaseUser = "root";
+        String databasePassword = "123456";
+        String url = "jdbc:mysql://localhost:3306/confessit";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+
+        return databaseLink;
+    }
 }
