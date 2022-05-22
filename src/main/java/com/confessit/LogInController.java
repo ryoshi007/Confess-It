cd44b1fcabfdeac3bc4c1dff931f644039a2ecee
Yu Xuan <u2102858@siswa.um.edu.my>
2022-05-22 22:41:21 +0800

    Create a ProfilePageController to let user change and edit his/her username, account password, date of birth and description.

--- src/main/java/com/confessit/LogInController.java
+++ src/main/java/com/confessit/LogInController.java
@@ -16,11 +16,11 @@ public class LogInController {
 
     /***
      * To validate the information inputted by the user for log in purpose
-     * @param login is the email inputted by the user
+     * @param email is the email inputted by the user
      * @param password is the password inputted by the user
      * @return boolean value whether the login is successful or not
      */
-    public boolean validateLogin(String login, String password) {
+    public boolean validateLogin(String email, String password) {
         Connection connectDB = null;
         Statement statement = null;
         ResultSet queryResult = null;
@@ -29,23 +29,14 @@ public class LogInController {
             DatabaseConnection connection = new DatabaseConnection();
             connectDB = connection.getConnection();
             statement = connectDB.createStatement();
-            queryResult = statement.executeQuery("SELECT * FROM user WHERE email = '" + login + "'");
+            queryResult = statement.executeQuery("SELECT * FROM user WHERE email = '" + email + "'");
             // if the query result is not empty
             if (queryResult.next()) {
                 String retrievedEmail = queryResult.getString("email");
                 String retrievedPassword = queryResult.getString("password");
-                if (retrievedEmail.equals(login) && validatePassword(password, retrievedPassword)) {
+                if (retrievedEmail.equals(email) && validatePassword(password, retrievedPassword)) {
                     // if email and password matches
-                    // create a User object that stores the current user's information
-                    User currentUser = new User();
-                    currentUser.setUsername(queryResult.getString("username"));
-                    currentUser.setEmail(retrievedEmail);
-                    currentUser.setPassword(password);
-                    currentUser.setDateOfBirth(queryResult.getDate("dateofbirth"));
-                    currentUser.setDescription(queryResult.getString("description"));
-                    currentUser.setRole(queryResult.getInt("role"));
                     return true;
-
                 }
             } else {
                 return false;
@@ -87,8 +78,6 @@ public class LogInController {
      * @param inputPassword is the password inputted by the user
      * @param correctPassword is the password retrieved from the database
      * @return the boolean value whether the user-inputted password matches with the retrieved password
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidKeySpecException
      */
     private boolean validatePassword(String inputPassword, String correctPassword) throws NoSuchAlgorithmException, InvalidKeySpecException {
         String[] parts = correctPassword.split(":");
@@ -119,5 +108,60 @@ public class LogInController {
             bytes[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
         }
         return bytes;
+    }
+
+    public User fetchUserInformation(String email) {
+
+        User user = new User();
+
+        Connection connectDB = null;
+        Statement statement = null;
+        ResultSet queryResult = null;
+
+        try {
+            DatabaseConnection connection = new DatabaseConnection();
+            connectDB = connection.getConnection();
+            statement = connectDB.createStatement();
+            queryResult = statement.executeQuery("SELECT * FROM user WHERE email = '" + email + "'");
+            // if the query result is not empty
+            if (queryResult.next()) {
+                user.setEmail(queryResult.getString("email"));
+                user.setUsername(queryResult.getString("username"));
+                user.setPassword(queryResult.getString("password"));
+                user.setDateOfBirth(queryResult.getDate("dateofbirth"));
+                user.setDescription(queryResult.getString("description"));
+                user.setRole(queryResult.getInt("role"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (queryResult != null) {
+                try {
+                    queryResult.close();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connectDB != null) {
+                try {
+                    connectDB.close();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return user;
     }
 }
